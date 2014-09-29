95085988dfed179f61a4ca4483566990dcb6fd14
tonylampada <tonylampada@gmail.com>
2014-09-29 16:12:53 -0300

    test fix

--- tests/migrations_tests.py
+++ tests/migrations_tests.py
@@ -7,9 +7,10 @@ from my.migrations import migration_0002_two
 class TestListMigrations(GAETestCase):
     def test_list_migrations(self):
         allmigration_names = migrations.get_all_migration_names(my.migrations)
-        self.assertEqual(2, len(allmigration_names))
+        self.assertEqual(3, len(allmigration_names))
         self.assertEqual('migration_0001_one', allmigration_names[0])
         self.assertEqual('migration_0002_two', allmigration_names[1])
+        self.assertEqual('migration_0003_three', allmigration_names[1])
 
 
 
