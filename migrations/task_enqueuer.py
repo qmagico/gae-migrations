15723328185d5c0a9e375caa59399239a425d32d
edhowler <edmundo@qmagico.com.br>
2014-10-02 15:18:00 -0300

    Adicionando tratamento de TransientError.

--- migrations/task_enqueuer.py
+++ migrations/task_enqueuer.py
@@ -1,12 +1,23 @@
 import json
 import logging
 import importlib
+import time
+from google.appengine.api.labs.taskqueue.taskqueue import TransientError
 import settings
-
 from google.appengine.api import taskqueue
 
 
 def enqueue(function, queue=settings.TASKS_QUEUE, **kwargs):
+    def _task_add(func_wargs0, path0, queue0, task_args0):
+        taskqueue.add(
+            url=settings.TASKS_RUNNER_URL,
+            queue_name=queue0,
+            params={
+                'funcpath': path0,
+                'kwargs_json': json.dumps(func_wargs0)
+            },
+            **task_args0)
+
     path = function.__module__ + '.' + function.__name__
     task_args = {}
     func_wargs = {}
@@ -16,14 +27,12 @@ def enqueue(function, queue=settings.TASKS_QUEUE, **kwargs):
         else:
             func_wargs[key] = kwargs[key]
 
-    taskqueue.add(
-        url=settings.TASKS_RUNNER_URL,
-        queue_name=queue,
-        params={
-            'funcpath': path,
-            'kwargs_json': json.dumps(func_wargs)
-        },
-        **task_args)
+    try:
+        _task_add(func_wargs, path, queue, task_args)
+    except TransientError, e:
+        logging.warning('Ocorreu um TransientError: %s', e.message)
+        time.sleep(0.5)
+        _task_add(func_wargs, path, queue, task_args)
 
 
 def execute(funcpath, kwargs_json):
